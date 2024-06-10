69f99fa3a3601ad675e7ce79e4557be6dc4c0515
RemiTZU <bonnetremi74@gmail.com>
2024-06-10 19:14:52 +0200

    carrousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
-
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturesContainerComponent } from './features-container/features-container.component';
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    FeaturesContainerComponent
   ],
   imports: [
     BrowserModule,
